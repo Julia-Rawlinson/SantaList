fbf1dc934ed1c0bcd0d809931914a0eede3a5c2b
Julia Rawlinson <rawlinsonjulia@gmail.com>
2021-11-26 11:25:00 -0500

    Added database

--- SantaList/Models/AppUsers.cs
+++ SantaList/Models/AppUsers.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SantaList.Models
 {
-    public class AppUser
+    public class AppUsers : IdentityUser
     {
     }
 }
--- SantaList/Startup.cs
+++ SantaList/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SantaList.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +18,18 @@ namespace SantaList
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public IConfiguration Configuration { get; }
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<SantaListDbContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:DbConnStr"]));
+            services.AddIdentity<AppUsers, IdentityRole>()
+                .AddEntityFrameworkStores<SantaListDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +49,12 @@ namespace SantaList
 
             app.UseStaticFiles();
 
+            //These must be in this order for the  the middleware to work correctly
             app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
